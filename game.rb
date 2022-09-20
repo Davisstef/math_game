aa149d62443814b962638a5f722263c0c05f841f
Stefan Darrell Davis <60591525+Davisstef@users.noreply.github.com>
2022-09-19 23:44:46 -0400

    Update game.rb

--- game.rb
+++ game.rb
@@ -1,27 +1,55 @@
-require './player'
 
-class Game
+class Game 
+  SCORE = 3
+  @@current_turn = 1
+  
+  def initialize(player1, player2, turn)
+    @player1 = player1
+    @player2 = player2
+    @turn = turn
 
-  @@player = 0;
-  def start(players)
-    puts "----- NEW TURN -----"
-    question = Question.new
-    puts "#{players[0].long}: #{question.qs}"
-    print "> "
-    answer = $stdin.gets.chomp
-    if answer.to_i == question.number1 + question.number2
-      puts "#{players[0].long}: YES! You're correct."
-    else
-      players[0].loose_points
-      puts "#{players[0].long}: Seriously? No!"
-    end
+  end
+  def play
+    p1_score = SCORE
+    p2_score = SCORE
 
-    if players[0].score == 0
-      puts "#{players[1].long} wins  with a score of #{players[1].final_score} \n----- GAME OVER -----\nGood Bye"
-    else
-      players.reverse!
-      "#{players[1].short}: #{players[1].final_score} vs #{players[0].short}: #{players[0].final_score}"
-      start(players)
-    end
+    while @turn.player.score > 0 
+      @turn.ask_question 
+      @turn.check_answer
+
+      @@current_turn += 1
+      # counting the score
+      if @turn.player.name == "Player 1" 
+        p1_score = @turn.player.score
+      else
+        p2_score = @turn.player.score
+      end
+
+      puts "P1: #{p1_score}/#{SCORE} vs P2: #{p2_score}/#{SCORE}"
+
+      
+
+      # Game Over message
+      if @turn.player.score == 0
+        # Display the winner
+         puts @turn.player.name == "Player1" ? "player2 wins with a score of #{p2_score}/#{SCORE}" : "player1 wins with a score of #{p1_score}/#{SCORE}"
+
+        puts "--Game Over--"
+        puts "GoodBye"
+        exit(0)
+      end
+
+        # To change the player for next turn
+      puts "--Next Turn--"
+      if @@current_turn % 2 == 0 
+          @turn.player = @player2
+      else
+        # current_player = @player1
+        @turn.player = @player1
+      end  
+     
+      
+   end
+
   end
 end
