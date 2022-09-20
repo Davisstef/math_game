ed2ea75eb844519d98bda9dbb558ddeb5515b0f2
Stefan Darrell Davis <60591525+Davisstef@users.noreply.github.com>
2022-09-19 23:47:26 -0400

    Update player.rb

--- player.rb
+++ player.rb
@@ -1,24 +1,8 @@
-require "pry"
 class Player
-  @@number_of_players = 0
-
-  attr_reader :score, :short, :long
-
-  def initialize
-    @@number_of_players +=1
-    self.short = "P#{@@number_of_players}"
-    self.long = "Player#{@@number_of_players}"
-    self.score = 3
-  end 
-
-  def loose_points
-    self.score -= 1
+  attr_accessor :score, :name
+  def initialize(name)
+    @name = name
+    @score = 3
   end
 
-  def final_score
-    "#{score}/3"
-  end
-
-  protected
-  attr_writer :score, :short, :long
 end
