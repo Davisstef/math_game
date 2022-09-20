fcb1bf6f7be355e62c37aa4f846deb6be63eddfb
Stefan Darrell Davis <60591525+Davisstef@users.noreply.github.com>
2022-09-19 23:59:36 -0400

    Create e7.rb

---
+++ exercises/e7.rb
@@ -0,0 +1,22 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+
+puts "Exercise 7"
+puts "----------"
+
+puts "Please enter a updated store name:"
+
+@update_store_name = gets.chomp
+
+@store = Store.create(name: @update_store_name)
+
+if @store.errors.any?
+  @store.errors.full_messages.each do |message|
+    puts message
+  end
+end
